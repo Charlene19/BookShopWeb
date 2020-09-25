d32d389dbfd81cb84ef08a51ce41cc8cd67d372c
Charlène <Charlne@Charlne-PC>
2020-09-25 16:42:59 +0200

    n el

---
+++ src/java/beans/beanBasket.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import java.beans.*;
+import java.io.Serializable;
+
+/**
+ *
+ * @author cda611
+ */
+public class beanBasket implements Serializable {
+    
+    
+    
+    public beanBasket() {
+        //HashMap 
+       
+    }
+    
+   /* Permet l'ajout, le retrait de produit dans le panier. */
+  
+   public void  addProduct(){  
+   }
+   
+   public void  deleteProduct(){
+       
+   }
+   
+   public void  clear(){
+       
+   }
+   
+ // public Order buyBasket(){}
+  
+ //public List allBasket(){}
+   
+}
